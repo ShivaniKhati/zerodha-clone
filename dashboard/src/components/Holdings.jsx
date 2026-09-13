db80adce5d59c8ac3cf40afc6da450bf1486fd50
Shivani <Shivi@123gmail.com>
2026-09-13 08:54:24 +0530

    Connect holdings to production API

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -28,7 +28,7 @@ const Holdings = () => {
    const [allHoldings, setAllHoldings] = useState([]);
     
    useEffect(() => {
-      axios.get("http://localhost:3002/allHoldings").then((res) => {
+      axios.get("https://zerodha-clones-7rvl.onrender.com/allHoldings").then((res) => {
          console.log(res.data);
         setAllHoldings(res.data);
       });
